b3f2c8951116159e5f750b548b366a1b88a418d8
vipin m <vipinm500@gmail.com>
2023-07-18 10:06:54 +0100

    completed todo app

---
+++ src/app/mock-task.ts
@@ -0,0 +1,21 @@
+import {Task} from './Task'
+export const TASKS:Task[]=[
+    {
+        id:1,
+        text:'Doctors Appointment',
+        day:'MAY 5th at 2.30pm',
+        reminder:true
+    },
+    {
+        id:2,
+        text:'meeting at scool',
+        day:'MAY 6th at 1.30pm',
+        reminder:true
+    },
+    {
+        id:3,
+        text:'Food shoping',
+        day:'MAY 7th at 12.30pm',
+        reminder:true
+    }
+]
